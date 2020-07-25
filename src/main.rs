e5f45c9c043cfed65bd9bedad0c2a2a38a6e301c
Gowtham Gutha <gowthamgutha@github.com>
2020-07-25 13:14:28 +0530

    Add sum

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,13 @@ fn hello(name: String, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
+#[get("/sum/<a>/<b>")]
+fn sum(a: i32, b: i32) -> String {
+    format!("{} + {} = {}", a, b, a+b)
+}
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite().mount("/", routes![hello, sum])
+    .launch();
 }
